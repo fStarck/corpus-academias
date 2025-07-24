39f22dde7ffe6a1d17382014bfc7d76183bde7f3
fStarck <felipegstarck@gmail.com>
2025-07-23 21:50:15 -0300

    Updates

---
+++ precache-manifest.35c4cb6071232f5a46bb8b021be45132.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583aecec73f1cfab21fa979dfefae318",
+    "url": "/corpus-academias/index.html"
+  },
+  {
+    "revision": "ef12dcba55999dc09af3",
+    "url": "/corpus-academias/static/css/2.b0969361.chunk.css"
+  },
+  {
+    "revision": "4c740d0e233ffbf86514",
+    "url": "/corpus-academias/static/css/main.64d41250.chunk.css"
+  },
+  {
+    "revision": "ef12dcba55999dc09af3",
+    "url": "/corpus-academias/static/js/2.dd305b1a.chunk.js"
+  },
+  {
+    "revision": "4c740d0e233ffbf86514",
+    "url": "/corpus-academias/static/js/main.b26bef82.chunk.js"
+  },
+  {
+    "revision": "0269bd1afa5f292a89b4",
+    "url": "/corpus-academias/static/js/runtime~main.670e9524.js"
+  },
+  {
+    "revision": "5824d0835ab2c56e1ee364e15d84c784",
+    "url": "/corpus-academias/static/media/Bozon-Regular.5824d083.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/corpus-academias/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/corpus-academias/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/corpus-academias/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/corpus-academias/static/media/slick.f97e3bbf.svg"
+  }
+]);

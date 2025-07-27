7dd5cd08cf8f8d352898eecf5d7d751a7f90684a
fStarck <felipegstarck@gmail.com>
2025-07-27 11:06:28 -0300

    Updates

---
+++ precache-manifest.03c53c683f704fe565f2e7a4d9ee9d3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8006eef5526f6dd803c58b9fdb5fdf4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0beb818c2e7b293427f",
+    "url": "/static/css/2.48d95ff0.chunk.css"
+  },
+  {
+    "revision": "6c28afd0def7573d618e",
+    "url": "/static/css/main.f35a07ce.chunk.css"
+  },
+  {
+    "revision": "e0beb818c2e7b293427f",
+    "url": "/static/js/2.dd305b1a.chunk.js"
+  },
+  {
+    "revision": "6c28afd0def7573d618e",
+    "url": "/static/js/main.b26bef82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5824d0835ab2c56e1ee364e15d84c784",
+    "url": "/static/media/Bozon-Regular.5824d083.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corpus-academias/precache-manifest.35c4cb6071232f5a46bb8b021be45132.js"
+  "/precache-manifest.03c53c683f704fe565f2e7a4d9ee9d3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corpus-academias/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
